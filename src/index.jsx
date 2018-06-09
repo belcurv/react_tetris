01947c264553efe9f7f9a780dd5787dacfe41572
Jay Schwane <jrschwane@uwalumni.com>
2018-06-09 10:27:30 -0500

    Updates root index.jsx with new model & components; instances two shapes

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const Demo = () => (
-  <div>Is this thing on?</div>
-);
+import * as Components from './components/components';
+import * as Model from './models/models';
+
+import './styles/style.css';
+
+const data = [
+  new Model.O(1, 1),
+  new Model.I(1, 4)
+];
+
 
 ReactDOM.render(
-  <Demo />,
+  <div>
+    { data.map((c, i) => <Components.ShapeView key={ i } shape={ c } />)}
+  </div>,
   document.getElementById('app')
 );
