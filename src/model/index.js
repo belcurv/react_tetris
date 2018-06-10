ca118fd9ff7a067aa252f93de7c597718daa6443
Jay Schwane <jrschwane@uwalumni.com>
2018-06-10 10:13:53 -0500

    Adds some notes, yay

--- src/model/index.js
+++ src/model/index.js
@@ -1,4 +1,8 @@
-export { default as Game } from './Game';
+/**
+ * Just re-exports the default exports of each individual model class
+*/
+
+export { default as Game }  from './Game';
 export { default as Piece } from './Piece';
 export { default as Point } from './Point';
 export { default as Shape } from './Shape';
