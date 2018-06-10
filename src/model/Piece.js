c825987ecacdcc90e2851dee65ae8ac163cb85ba
Jay Schwane <jrschwane@uwalumni.com>
2018-06-10 16:18:53 -0500

    Adds utility methods related to movement and rotation

--- src/model/Piece.js
+++ src/model/Piece.js
@@ -17,14 +17,57 @@ export default class Piece {
       .map((point) => point.addOffset(this.offset));
   }
 
+  maxRow() {
+    let pointRows = this.points().map(point => point.row);
+    return Math.max(...pointRows);
+  }
+
+  maxCol() {
+    let pointCols = this.points().map(point => point.col);
+    return Math.max(...pointCols);
+  }
+
+  minCol() {
+    let pointCols = this.points().map(point => point.col);
+    return Math.min(...pointCols);
+  }
+
+  hasPoint(point) {
+    return this.points()
+      .filter(item => item.sameAs(point))
+      .length > 0;
+  }
+
+  rotate() {
+    let currentRotIndex = Piece.rotations().indexOf(this.rotation);
+    this.rotation = Piece.rotations()[(currentRotIndex + 1) % 4];
+  }
+
+  unRotate() {
+    let currentRotIndex = Piece.rotations().indexOf(this.rotation);
+    this.rotation = Piece.rotations()[(currentRotIndex - 1) % 4];
+  }
+
+  fallOne() {
+    this.offset = new Point(this.offset.row + 1, this.offset.col);
+  }
+
+  liftOne() {
+    this.offset = new Point(this.offset.row - 1, this.offset.col);
+  }
+
+  left() {
+    this.offset = new Point(this.offset.row, this.offset.col - 1);
+  }
+
+  right() {
+    this.offset = new Point(this.offset.row, this.offset.col + 1);
+  }
+
   // set of all possible rotations
   // static methods are accessible by the class; not by instances
   static rotations() {
     return ['N', 'E', 'S', 'W'];
   }
 
-  maxRow() {
-    let pointRows = this.points().map(point => point.row);
-    return Math.max(...pointRows);
-  }
 }
