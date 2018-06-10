6150f3b82abdc8f429de18f04de14f90a7970e53
Jay Schwane <jrschwane@uwalumni.com>
2018-06-10 16:18:31 -0500

    Adds ROTATE, LEFT and RIGHT actions to reducer

--- src/reducers/tetris.js
+++ src/reducers/tetris.js
@@ -1,3 +1,17 @@
+/**
+ * WTF is the author doing.
+ * 
+ * State should look like this:
+ * cols: 10
+ * rows: 20
+ * fallingPiece: {
+ *   offset: { row: Number, col: Number},
+ *   rotation: String
+ *   shape: { name: "String", rotator: function }
+ * }
+ * rubble: [ { row: Number, col: Number } ]
+*/
+
 /* ================================= SETUP ================================= */
 
 import * as Model from '../model';
@@ -10,8 +24,16 @@ export default (state = defaultState, action) => {
   switch (action.type) {
 
     case 'TICK':
-      console.log('TICK!');
       return state.tick();
+
+    case 'ROTATE':
+      return state.rotate();
+
+    case 'LEFT':
+      return state.left();
+
+    case 'RIGHT':
+      return state.right();
 
     default:
       return state;
