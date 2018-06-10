3873dfeb79963f592ddc6f698130dfab784c373f
Jay Schwane <jrschwane@uwalumni.com>
2018-06-10 16:16:45 -0500

    Adds Mousetrap key bindings

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,38 @@
+/* ================================= SETUP ================================= */
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Mousetrap from 'mousetrap';
+
 import { GameView } from './components/components';
 import configureStore from './store/configureStore';
 
 import './styles/style.css';
 
 const appRoot = document.getElementById('app');
+
+
+/* ============================= CONFIG STORE ============================== */
+
 const store = configureStore();
+
+
+/* ============================== BIND EVENTS ============================== */
+
+Mousetrap.bind('space', () => store.dispatch({ type: 'ROTATE' }));
+Mousetrap.bind('left',  () => store.dispatch({ type: 'LEFT' }));
+Mousetrap.bind('right', () => store.dispatch({ type: 'RIGHT' }));
+
+
+/* ================================ STARTUP ================================ */
 
 store.subscribe(() => {
   const state = store.getState();
-  console.log(state);
+  // console.log(state.rubble);
   ReactDOM.render(
     <GameView game={ state } />,
     appRoot
   );
 });
 
-setInterval(() => store.dispatch({ type: 'TICK' }), 1000);
+setInterval(() => store.dispatch({ type: 'TICK' }), 500);
