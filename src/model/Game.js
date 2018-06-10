8634ee3e1d81a8b38fc8d40175b711c726bc6aaf
Jay Schwane <jrschwane@uwalumni.com>
2018-06-10 16:19:19 -0500

    Adds game model methods related to movement and rotation

--- src/model/Game.js
+++ src/model/Game.js
@@ -1,5 +1,6 @@
 import Piece from './Piece';
 import shapes from './shapes';
+import Point from './Point';
 
 /**
  * the Game. Tracks currently falling piece and the 'rubble' left by all the
@@ -19,8 +20,17 @@ export default class Game {
   }
 
   tick() {
-    this.fallingPiece.offset = this.fallingPiece.offset.fallOne();
+    this.attempt(
+      () => this.fallingPiece.fallOne(),
+      () => this.fallingPiece.liftOne()
+    );
     if (this.fallingPiece.maxRow() >= this.rows) {
+      this.convertToRubble();
+      return this;
+    }
+    let nextPos = this.fallingPiece.points().map(p => new Point(p.row + 1, p.col));
+    if (nextPos.some(p => this.rubble.some(r => r.sameAs(p)))) {
+      console.log('I should convert to rubble');
       this.convertToRubble();
     }
     return this;
@@ -32,7 +42,44 @@ export default class Game {
   }
 
   rotate() {
-    this.fallingPiece.rotate();
+    this.attempt(
+      () => this.fallingPiece.rotate(),
+      () => this.fallingPiece.unRotate()
+    );
     return this;
   }
+
+  left() {
+    this.attempt(
+      () => this.fallingPiece.left(),
+      () => this.fallingPiece.right()
+    );
+    return this;
+  }
+
+  right() {
+    this.attempt(
+      () => this.fallingPiece.right(),
+      () => this.fallingPiece.left()
+    );
+    return this;
+  }
+
+  fallingPieceOutOfBounds() {
+    return this.fallingPiece.minCol() < 1
+      || this.fallingPiece.maxCol() > this.cols
+      || this.fallingPiece.maxRow() > this.rows;
+  }
+
+  fallingPieceOverlapsRubble() {
+    return this.fallingPiece.points().some(p => this.rubble.some(r => r.sameAs(p)));
+  }
+
+  attempt(action, revertAction) {
+    action();
+    if (this.fallingPieceOutOfBounds() || this.fallingPieceOverlapsRubble()) {
+      revertAction();
+    }
+  }
+
 }
