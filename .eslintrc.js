2e14717cf0f5526ff23ec03404db32a98efd2e10
Jay Schwane <jrschwane@uwalumni.com>
2018-06-10 16:15:46 -0500

    Reformat eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,44 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+    'jest': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
     },
-    "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended"
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'no-console': 0,
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 }
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "rules": {
-        "no-console": 0,
-        "indent": [
-            "error",
-            2,
-            { "SwitchCase": 1 }
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "react/jsx-uses-vars": [2]
-    }
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'react/jsx-uses-vars': [2]
+  }
 };
